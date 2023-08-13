162f0e13f3997e38a2622f727678be935badde9f
Silviu D. T <silviutanasie@gmail.com>
2023-08-13 23:47:24 +0300

    Update install_anydesk.sh

--- fedora38/install_anydesk.sh
+++ fedora38/install_anydesk.sh
@@ -1,1 +1,18 @@
-
+#create Anydesk repo in repo folder
+sudo "[anydesk]\nname=AnyDesk Fedora - stable\nbaseurl=http://rpm.anydesk.com/fedora/x86_64/\ngpgcheck=0\nrepo_gpgcheck=1\ngpgkey=https://keys.anydesk.com/repos/RPM-GPG-KEY" > /etc/yum.repos.d/AnyDesk-Fedora.repo
+sudo dnf makecache
+#installing dependencies
+sudo dnf install redhat-lsb-core
+sudo dnf --releasever=32 install pangox-compat.x86_64
+sudo dnf install mesa-libGLU
+sudo dnf install gtkglext-libs
+#fixing some library paths hardcoded in Anydesk that don't match with Fedora
+sudo ln -s /lib64/libgtkglext-x11-1.0.so.0 /lib64/libgtkglext-x11-1_0-0.so.0
+sudo echo "/usr/lib64/gtk-3.0/modules/" > /etc/ld.so.conf.d/pk-gtk.conf
+sudo ldconfig 
+#finally installing anydesk
+sudo dnf -y install anydesk
+#running anydesk
+#deleting old user any desk folder if it exists
+sudo rm -rf $HOME/.anydesk
+anydesk
