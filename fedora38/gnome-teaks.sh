087616d06c22b7bcc0984ef12e3d4ba97140dcc6
Silviu D. T <silviutanasie@gmail.com>
2023-09-15 11:12:49 +0300

    Update gnome-teaks.sh

--- fedora38/gnome-teaks.sh
+++ fedora38/gnome-teaks.sh
@@ -2,26 +2,7 @@
 
 # 0. Install Gnome Tweak Tools
 sudo dnf install gnome-tweak-tool
+# 1. Install Dash-to-Dock
+sudo dnf install gnome-shell-extension-dash-to-dock
 
-uuid=dash-to-dock@micxgx.gmail.com
-pk=307
-
-# 1. GNOME shell version
-shell_version=$(gnome-shell --version | cut -d' ' -f3)
-
-# 2. Fetch extension info (for the given shell version)
-info_json=$(curl -sS "https://extensions.gnome.org/extension-info/?uuid=$uuid&shell_version=$shell_version")
-# 2.a instead of ?uuid=$uuid you can use ?pk=$pk
-
-# 3. Extract download url from the json with jq
-download_url=$(echo $info_json | jq ".download_url" --raw-output)
-
-# 4. Install the extension
-gnome-extensions install "https://extensions.gnome.org$download_url"
-# 4.a ... or download it first, then install
-curl -sL "https://extensions.gnome.org$download_url" -o $uuid.zip
-gnome-extensions install $uuid.zip
-# 4.a.i ... or manually extract the zip
-mkdir -p ~/.local/share/gnome-shell/extensions/$uuid
-unzip $uuid.zip -d ~/.local/share/gnome-shell/extensions/$uuid
 
