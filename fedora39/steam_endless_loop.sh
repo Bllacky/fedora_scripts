c29a2ed3c88eef6e0152136655156b9b3348d36a
Silviu D. T <silviutanasie@gmail.com>
2023-11-12 01:47:09 +0200

    Update steam_endless_loop.sh

--- fedora39/steam_endless_loop.sh
+++ fedora39/steam_endless_loop.sh
@@ -1,1 +1,5 @@
-adas
+#!/usr/bin/env bash
+
+#If Steam is stuck in an endless loop of starting up and crashing, it happens mostly due to its new interface.
+#The easiest solution is to clean up the local cache
+sudo rm -d -R ~/.local/share/Steam
