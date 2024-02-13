271cc6705219caa92840390b2f1e4c546b417cb0
Silviu D. T <silviutanasie@gmail.com>
2024-02-13 10:49:05 +0200

    Update devel-libs.sh

--- fedora39/devel-libs.sh
+++ fedora39/devel-libs.sh
@@ -1,2 +1,2 @@
 #dependencies that are needed to update Python packages
-dnf install httpd-devel python3-devel cmake ninja-build libxml2-devel cairo-dev
+dnf install httpd-devel python3-devel cmake ninja-build libxml2-devel cairo-devel
