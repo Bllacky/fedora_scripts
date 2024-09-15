3e79c82f54cfe9843ef8e133e02c4a8a6857bd20
Silviu D. T. <silviutanasie@gmail.com>
2024-09-15 12:05:03 +0300

    Update devel-libs.sh

--- fedora40/devel-libs.sh
+++ fedora40/devel-libs.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 #dependencies that are needed to update Python packages
+dnf install gcc-c++
 dnf install httpd-devel python3-devel cmake ninja-build libxml2-devel cairo-devel gobject-introspection-devel cairo-gobject-devel cups-devel libxkbcommon-devel wxBase3-devel wxGTK3-devel gtk3-devel
 
 exit
