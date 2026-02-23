a2380f9adde64dffdaac61cf7bf0cce13cf5fbf9
Silviu D. T. <silviutanasie@gmail.com>
2026-02-23 10:57:34 +0200

    Create prevent_window_overlap_with_dock.sh

---
+++ fedora43/prevent_window_overlap_with_dock.sh
@@ -0,0 +1,3 @@
+gsettings set org.gnome.shell.extensions.dash-to-dock dock-fixed true
+gsettings set org.gnome.shell.extensions.dash-to-dock autohide false
+gsettings set org.gnome.shell.extensions.dash-to-dock intellihide false
